9fc1f833809eb4740bd8e77ca9729fc34dc7d87f
Samuel Belo <belo.samuel@gmail.com>
2025-02-01 11:05:52 -0300

    validation for user and custom response function

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -4,39 +4,53 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repositories"
+	"api/src/responses"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+
+	"github.com/go-playground/validator/v10"
 )
+
+var validate *validator.Validate
+
+func init() {
+	validate = validator.New()
+}
 
 func UserCreate(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, "Failed to read request body", http.StatusBadRequest)
+		responses.JsonResponse(w, http.StatusBadRequest, map[string]string{"error": "Failed to read request body"})
 		return
 	}
 
 	var user models.User
 	if err = json.Unmarshal(body, &user); err != nil {
-		http.Error(w, "Failed to unmarshal JSON", http.StatusBadRequest)
+		responses.JsonResponse(w, http.StatusBadRequest, map[string]string{"error": "Failed to unmarshal JSON"})
+		return
+	}
+
+	if err := validate.Struct(user); err != nil {
+		responses.JsonResponse(w, http.StatusBadRequest, map[string]string{"error": fmt.Sprintf("Validation failed: %v", err)})
 		return
 	}
 
 	db, err := database.Connect()
 	if err != nil {
-		http.Error(w, "Failed to connect to database", http.StatusInternalServerError)
+		responses.JsonResponse(w, http.StatusInternalServerError, map[string]string{"error": "Failed to connect to database"})
 		return
 	}
 
 	repository := repositories.NewUsersRepository(db)
 	userId, err := repository.Create(user)
 	if err != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		responses.JsonResponse(w, http.StatusInternalServerError, map[string]string{"error": "Failed to create user"})
 		return
 	}
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf("User created: %s", userId)))
+
+	responses.JsonResponse(w, http.StatusCreated, map[string]interface{}{"message": "User created successfully", "user_id": userId})
 }
 
 func UserGetAll(w http.ResponseWriter, r *http.Request) {
--- src/models/user.go
+++ src/models/user.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `json:"id,omitempty"`
-	Name      string    `json:"name,omitempty"`
-	Email     string    `json:"email,omitempty"`
-	Password  string    `json:"password,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
+	ID        uuid.UUID `json:"id" validate:"-"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email" validate:"required,email"`
+	Password  string    `json:"password" validate:"required,min=8"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
